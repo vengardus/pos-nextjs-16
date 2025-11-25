10924b892843ec6c383cb5a25cc5be22894225f3
ismytv <ismytv@gmail.com>
2025-11-24 23:07:45 -0500

    test(product): cambia a unstable cache

--- src/lib/data/products/product.cache.ts
+++ src/lib/data/products/product.cache.ts
@@ -1,34 +1,35 @@
-"use cache";
+//"use cache";
 
 import 'server-only'
 
 import {
-  cacheLife,
-  cacheTag,
+  //cacheLife,
+  //cacheTag,
+  unstable_cache as cache,
 } from "next/cache";
 
 import { CacheConfig } from "@/config/cache.config";
 import type { ResponseAction } from "@/types/interfaces/common/response-action.interface";
 import { productGetAllByCompany } from "./product.get-all-by-company";
 
-export const productGetAllByCompanyCached = async (
-  companyId: string
-): Promise<ResponseAction> => {
-  cacheTag(`products-${companyId}`);
-  cacheLife(CacheConfig.CacheDurations);
-  console.log("products-cached", `products-${companyId}`);
-  return await productGetAllByCompany(companyId);
-};
+// export const productGetAllByCompanyCached = async (
+//   companyId: string
+// ): Promise<ResponseAction> => {
+//   cacheTag(`products-${companyId}`);
+//   cacheLife(CacheConfig.CacheDurations);
+//   console.log("products-cached", `products-${companyId}`);
+//   return await productGetAllByCompany(companyId);
+// };
 
-// export async function productGetAllByCompanyCachedOld(companyId: string): Promise<ResponseAction> {
-//   // Aquí companyId está en scope, así que podemos usarlo en keyParts
-//   console.log("PRE-", `products-${companyId}`);
-//   const fn = cache(
-//     async () => {
-//       return productGetAllByCompany(companyId);
-//     },
-//     [`products-${companyId}`], // ahora sí existe
-//     { revalidate: CacheConfig.CacheDurations.revalidate }
-//   );
-//   return fn();
-// }
+export async function productGetAllByCompanyCached(companyId: string): Promise<ResponseAction> {
+  // Aquí companyId está en scope, así que podemos usarlo en keyParts
+  console.log("pre", `products-${companyId}`);
+  const fn = cache(
+    async () => {
+      return productGetAllByCompany(companyId);
+    },
+    [`products-${companyId}`], // ahora sí existe
+    { revalidate: CacheConfig.CacheDurations.revalidate }
+  );
+  return fn();
+}

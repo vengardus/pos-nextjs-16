c506bd4c87508b700c253a053b73d6c91af7d1f4
Vengardus <ismytv@gmail.com>
2025-12-21 21:18:26 -0500

    refactor(sale): mover acciones a arquitectura limpia

---
+++ src/server/modules/sale/next/actions/sale.insert.action.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { revalidatePath, updateTag } from "next/cache";
+import type { ResponseAction } from "@/types/interfaces/common/response-action.interface";
+import type { CartProduct } from "@/types/interfaces/sales/cart-product.interface";
+import type { Sale } from "@/types/interfaces/sales/sale.interface";
+import type { PosPayment } from "@/types/interfaces/pos-payment/pos-payment.interface";
+import { saleInsertUseCase } from "@/server/modules/sale/use-cases/sale.insert.use-case";
+
+export const saleInsertAction = async (
+  cartProducts: CartProduct[],
+  sale: Sale,
+  posPayment: PosPayment
+): Promise<ResponseAction> => {
+  if (!sale || !posPayment) {
+    return {
+      success: false,
+      message: "Datos de venta inválidos.",
+      pagination: {
+        currentPage: 0,
+        totalPages: 0,
+      },
+    };
+  }
+
+  const resp = await saleInsertUseCase(cartProducts, sale, posPayment);
+
+  if (resp.success) {
+    console.log(
+      "=>insert.sale",
+      `cash-register-movements-totals-${
+        posPayment.cashRegisterClosureId.length
+          ? posPayment.cashRegisterClosureId
+          : sale.companyId
+      }`
+    );
+
+    console.log(`UPDATE_TAG:cash-register-movements-totals-${sale.companyId}`);
+
+    updateTag(`cash-register-movements-totals-${sale.companyId}`);
+    revalidatePath("/dashboard");
+  }
+
+  return resp;
+};

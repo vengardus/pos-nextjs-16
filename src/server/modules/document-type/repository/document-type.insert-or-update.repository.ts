f8160804ed1898c26a2fdec3ca9b51d9171a5f66
Vengardus <ismytv@gmail.com>
2025-12-21 20:11:27 -0500

    refactor(document-type): mover server actions a clean architecture

---
+++ src/server/modules/document-type/repository/document-type.insert-or-update.repository.ts
@@ -0,0 +1,37 @@
+import "server-only";
+
+import prisma from "@/server/db/prisma";
+import type { Branch } from "@/types/interfaces/branch/branch.interface";
+
+export const documentTypeInsertOrUpdateRepository = async (
+  branch: Branch,
+  userId: string
+): Promise<Branch> => {
+  const { id, CashRegister, ...rest } = branch;
+
+  if (id) {
+    return await prisma.branchModel.update({
+      where: {
+        id,
+      },
+      data: {
+        ...rest,
+      },
+    });
+  }
+
+  return await prisma.branchModel.create({
+    data: {
+      ...rest,
+      BranchUser: {
+        createMany: {
+          data: [
+            {
+              userId,
+            },
+          ],
+        },
+      },
+    },
+  });
+};

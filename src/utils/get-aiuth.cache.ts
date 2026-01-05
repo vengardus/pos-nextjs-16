f62a5e328755bf939cc707588e591c22b78b4459
Vengardus <ismytv@gmail.com>
2026-01-05 11:01:54 -0500

    refactor(auth): elimina duplicado de caché de sesión

--- src/utils/get-aiuth.cache.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-import { ResponseAction } from "@/types/interfaces/common/response-action.interface";
-import { authGetSessionUseCase } from "@/server/modules/auth/use-cases/auth.get-session.use-case";
-
-export async function getAuthCached(): Promise<ResponseAction> {
-  //"use cache";
-  return await authGetSessionUseCase();  
-}

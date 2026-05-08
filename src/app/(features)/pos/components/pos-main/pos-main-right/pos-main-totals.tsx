d4be651fba2aed01b39727658c8dfd6e87866626
vengardus <26282978+vengardus@users.noreply.github.com>
2026-05-08 17:34:04 -0500

    fix(pos): add missing import for PosActionsMenu

--- src/app/(features)/pos/components/pos-main/pos-main-right/pos-main-totals.tsx
+++ src/app/(features)/pos/components/pos-main/pos-main-right/pos-main-totals.tsx
@@ -2,6 +2,7 @@
 
 import { useCartStore } from "@/stores/cart/cart.store";
 import { PosMainTotalsButton } from "./pos-main-totals-button";
+import { PosActionsMenu } from "./pos-actions-menu";
 import { cn } from "@/utils/tailwind/cn";
 
 export const PosMainTotals = () => {
